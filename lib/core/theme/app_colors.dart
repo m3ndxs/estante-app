6473b18130c254a48ec283b03a52aa23c0a332b9
Leonardo Mendes <leonardo_mendes2002@hotmail.com>
2026-05-26 19:10:56 -0300

    feat: implement splash screen

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -2,4 +2,12 @@ import 'package:flutter/material.dart';
 
 abstract final class AppColors {
   static const primary = Color(0xFF3B6B4C);
+  static const title = Color(0xFF795548);
+  static const text = Color(0xFFD7CCC8);
+  static const splashBackground = LinearGradient(
+    colors: [
+      primary,
+      Color(0xFF5D9C74),
+    ]
+  );
 }
--- lib/features/splash/presentation/splash_page.dart
+++ lib/features/splash/presentation/splash_page.dart
@@ -1,3 +1,4 @@
+import 'package:estante_app/core/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -14,7 +15,7 @@ class _SplashPageState extends State<SplashPage> {
     super.initState();
 
     Future.delayed(
-      const Duration(seconds: 2), 
+      const Duration(seconds: 3),
       () {
         if (mounted) {
           context.go('/login');
@@ -22,11 +23,39 @@ class _SplashPageState extends State<SplashPage> {
       }
     );
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Splash Page')),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(gradient: AppColors.splashBackground),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(Icons.menu_book_sharp, size: 120, color: AppColors.text),
+              const SizedBox(height: 24,),
+              Text(
+                'ESTANTE',
+                style: Theme.of(
+                  context,
+                ).textTheme.headlineMedium?.copyWith(color: AppColors.title),
+              ),
+              const SizedBox(height: 8,),
+              Text(
+                'Seu acervo de leituras na palma da mão',
+                style: Theme.of(
+                  context,
+                ).textTheme.bodyMedium?.copyWith(color: AppColors.text),
+              ),
+              const SizedBox(height: 48,),
+              const CircularProgressIndicator(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
